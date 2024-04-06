25cc57c6a59de8120ab8342cb545fd43680e6a88
DaisyDiaz1 <166200562+DaisyDiaz1@users.noreply.github.com>
2024-04-05 23:59:32 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,96 @@
+// Se importan las librerias a utilizar
+import express from 'express';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+
+const port = 8080;
+const app = express();
+
+app.use(bodyParser.json());
+
+// Funcion encargada de leer el archivo DATA.JSON que contiene
+// algunos datos referentes a la API
+const readData = () => {
+    try {
+        const data = fs.readFileSync('data/data.json');
+        return JSON.parse(data);
+    }catch(error) {
+        console.log(error);
+        return { tacos: [] };
+    }
+};
+
+// Funcion encargada de escribir dentro del archivo DATA.JSON
+const writeData = (data) => {
+    try {
+        fs.writeFileSync('data/data.json', JSON.stringify(data));
+    }catch(error) {
+        console.log(error);
+    }
+}
+
+/**
+ * Creacion de rutas
+ * 1. Ruta principal, solo muestra un mensaje en el navegador
+ * 2. Ruta que muestra los datos de la API, los datos se obtienen mediante el
+ *    metodo GET
+ * 3. Ruta que muestra un valor en especifico mediante el id usando el metodo GET
+ * 4. Ruta encargada de almacenar un nuevo valor dentro de la API mediante el metodo
+ *    POST
+ * 5. Ruta encargada de modificar el valor de un dato mediante el id usando el metodo PUT
+ * 6. Ruta encargada de eliminar un valor mediante el id usando el metodo DELETE
+*/
+app.get('/', (req, res) => {
+    res.send('Welcome to the Tacos API');
+});
+
+app.get('/tacos', (req, res) => {
+    const data = readData();
+    res.json(data.tacos);
+});
+
+app.get('/tacos/:id', (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const taco = data.tacos.find((taco) => taco.id === id);
+    res.json(taco);
+});
+
+app.post('/tacos', (req, res) => {
+    const data = readData();
+    const body = req.body;
+    const newOrder = {
+        id: data.tacos.length + 1,
+        ...body,
+    };
+    data.tacos.push(newOrder);
+    writeData(data);
+    res.json(newOrder);
+});
+
+app.put('/tacos/:id', (req, res) => {
+    const data = readData();
+    const body = req.body;
+    const id = parseInt(req.params.id);
+    const tacoIndex = data.tacos.findIndex((taco) => taco.id === id);
+    data.tacos[tacoIndex] = {
+        ...data.tacos[tacoIndex],
+        ...body,
+    };
+
+    writeData(data);
+    res.json({ message: "Order updated successfully."})
+});
+
+app.delete('/tacos/:id', (req, res) => {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const tacoIndex = data.tacos.findIndex((taco) => taco.id === id);
+    data.tacos.splice(tacoIndex, 1);
+    writeData(data);
+    res.json({ message: "Order deleted successfully" });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
